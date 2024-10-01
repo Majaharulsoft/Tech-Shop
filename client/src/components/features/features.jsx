8f43d97f44770c77e243bd3e7999a0d565185c81
Majaharulsoft <majaharulr1@gmail.com>
2024-10-01 22:42:11 +0600

    update

--- client/src/components/features/features.jsx
+++ client/src/components/features/features.jsx
@@ -13,14 +13,14 @@ const Features = () => {
             <div className="container section">
                 <div className="row">
                     {FeatureList.map((item,i)=>{
-                        return (<div key={i} className="col-6 p-2 col-md-3 col-lg-3 col-sm-6">
+                        return (<div key={i} className="col-6 p-2 col-md-3 col-lg-3 col-sm-6  ">
                             <div className="card shadow-sm">
                                 <div className="card-body">
                                     <div className="row ">
-                                        <div className="col-5 ">
+                                        <div className="col-6 ">
                                             <img alt="img" className='w-100 h-100 rounded' src={item['img']} />
                                         </div>
-                                        <div className="col-7">
+                                        <div className="col-6">
                                             <h3 className="bodyXLarge">{item['name']}</h3>
                                             <span className="bodySmal">{item['description']}</span>
                                         </div>
