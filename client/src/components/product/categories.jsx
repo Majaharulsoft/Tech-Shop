8e594e612105d09897b6b1eceb3012a00c336b9a
Majaharulsoft <majaharulr1@gmail.com>
2024-10-06 23:55:39 +0600

    update Categories

--- client/src/components/product/categories.jsx
+++ client/src/components/product/categories.jsx
@@ -19,10 +19,10 @@ const Categories = () => {
                         <span className="bodySmal mb-5 text-center">Explore a World of Choices Across Our Most Popular <br />Shopping Categories </span>
                         {
                             CategoryList.map((item,i)=>{
-                                return ( <div key={i} className="col-6 col-lg-8r text-center col-md-8r p-2">
+                                return ( <div key={i} className="col-6 col-lg-8r text-center col-md-2 p-2">
                                     <Link to={`/by-category/${item['_id']}`} className="card h-100 rounded-3 bg-white">
                                         <div className="card-body">
-                                            <img alt="" className="w-75" src={item['categoryImg']} />
+                                            <img alt="" className="w-50" src={item['categoryImg']} />
                                             <p className="bodySmal mt-3">{item['categoryName']}</p>
                                         </div>
                                     </Link>
